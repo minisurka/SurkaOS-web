de69527df8150124051cd387af2450b2404489c7
minisurka <vvana1405@gmail.com>
2025-12-14 18:27:40 +0800

    Update 1.1 phone_sysToast.js

--- phone_sysToast.js
+++ phone_sysToast.js
@@ -20,7 +20,7 @@ function backspace_phone() {
 function call_phone() {
   let num = display_phone.innerText;
   display_phone.innerText = " ";
-  if (num != "") tb_system("SIM card not found");
+  if (num != "") tb_system("Нет сети");
 }
 
 function tb_system(message) {
@@ -202,3 +202,4 @@ function hideAllAlerts() {
       setTimeout(() => backdrop_alert.remove(), 500);
     });
 }
+
